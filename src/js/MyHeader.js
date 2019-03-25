2788ffa03786529a91e4b24a6951dd9269af449d
Bodya <bodya.save.dev@gmail.com>
2019-03-25 18:41:54 +0200

    update

--- src/js/MyHeader.js
+++ src/js/MyHeader.js
@@ -170,7 +170,7 @@ let ThingContainer = Vue.component('thing-container', {
 			<p class="description">{{ thing.description }}</p>
 			<p class="contact-title">Контакти для покупки</p>
 			<a href="https://www.instagram.com/xedrum_shop/" class="number">Instagram: @xedrum_shop</a>
-			<p class="number">+380 98 377-12-67</p>
+			<a href="tel:+380 98 377-12-67" class="number">+380 98 377-12-67</a>
 			<p class="number">+380 67 204-77-43</p>
 		</div>
 	</div>`,
